aff8d28caa82e69f0a5acdd62b19ebe99d9a95d5
DESKTOP-SGLEE\mrelse <mrelse84@naver.com>
2020-03-07 12:41:37 +0900

    Initial Commit

---
+++ src/test.java
@@ -0,0 +1,16 @@
+public class test {
+	public static void main(String[] args) {
+		int kor1, kor2;
+		int total;
+		float avg;
+		
+		kor1 = 50;
+		kor2 = 60;
+		
+		total = kor1 + kor2;
+		avg = total / 2;
+		
+		System.out.printf("total is %d\n",  total);
+		System.out.printf("avg is %f\n",  avg);
+	}
+}
